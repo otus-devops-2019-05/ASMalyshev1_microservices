734beb9216c431ff1c0c658be00d5268e194b0ec
Aleksey Malyshev <mail@asmalyshev.ru>
2019-08-15 20:45:11 +0300

    docker-4

---
+++ PS/17.ps1
@@ -0,0 +1,29 @@
+﻿# 17. Сетевое взаимодействие Docker контейнеров. Docker Compose. Тестирование образов 
+
+Clear-Host
+Set-Location $PSScriptRoot
+#return
+
+<#
+Создайте новую ветку в вашем microservices репозитории в организации DevOps 2019-05 для выполнения данного ДЗ. 
+Ветку назовите docker-4.
+#>
+
+$Location = (Get-Location).Path
+$GitRootFolder = 'D:\GitHub\ASMalyshev1_microservices'
+
+# Создаем новую ветку в нашем инфраструктурном репозитории для выполнения данного ДЗ.
+$branchName = 'docker-4'
+
+# Создаем новую ветку "$branchName"
+git branch $branchName
+
+# Переходим в ветку "$branchName"
+git checkout $branchName
+
+# Переходим в папку с DockerFile
+Set-Location $GitRootFolder\docker-monolith\
+# Выполняем сборку контейнера
+docker build -t reddit:latest .
+# Запускаем контейнер
+docker run --name reddit -d --network=host reddit:latest
