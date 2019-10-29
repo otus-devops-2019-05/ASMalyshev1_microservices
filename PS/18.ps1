671d775dda8f9c8178f2df2e8fcb4a0c45ddf5dc
Aleksey Malyshev <mail@asmalyshev.ru>
2019-10-29 10:33:52 +0300

    1

---
+++ PS/18.ps1
@@ -0,0 +1,28 @@
+﻿# 19. Устройство Gitlab CI. Построение процесса непрерывной интеграции
+
+Clear-Host
+Set-Location $PSScriptRoot
+#return
+
+$Location = (Get-Location).Path
+$GitRootFolder = 'D:\GitHub\ASMalyshev1_infra'
+
+# Создаем новую ветку в нашем инфраструктурном репозитории для выполнения данного ДЗ.
+$branchName = 'gitlab-ci-1'
+
+# Создаем новую ветку "$branchName"
+git branch $branchName
+
+# Переходим в ветку "$branchName"
+git checkout $branchName
+
+#Поднимите инфраструктуру, описанную в окружении stage
+Set-Location $GitRootFolder\terraform\stage
+<#
+terraform destroy -auto-approve=true
+#>
+<#
+terraform init
+terraform plan
+terraform apply -auto-approve=true
+#>
